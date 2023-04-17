ca5bd2d5294e2e96df853305ae0750d92a2da353
VegasAE <S3947138@student.rmit.edu.au>
2023-04-17 15:03:20 +1000

    created methods for list based spreadsheet

--- spreadsheet/arraySpreadsheet.py
+++ spreadsheet/arraySpreadsheet.py
@@ -13,120 +13,94 @@ from spreadsheet.baseSpreadsheet import BaseSpreadsheet
 class ArraySpreadsheet(BaseSpreadsheet):
 
     def __init__(self):
-        # TO BE IMPLEMENTED
-        pass
+        # set the rows and columns that were given by the base spreadsheet
+        self.numCols = 0
+        self.numRows = 0
 
 
     def buildSpreadsheet(self, lCells: [Cell]):
-        """
-        Construct the data structure to store nodes.
-        @param lCells: list of cells to be stored
-        """
+    
+        # get max number of rows / cols by finding the largest value for each cell.col and cell.row
+        # add 1 after finished iterating through so value is not an index value.
+        for cell in lCells:
+            if cell.row > self.numRows:
+                self.numRows = cell.row()
+            if cell.col > self.numCols:
+                self.numCols = cell.row()
+        self.numCols += 1
+        self.numRows += 1
 
-        # TO BE IMPLEMENTED
-        pass
+
+        # create an empty 2D list and set the value of each position using the cells position.
+        self.arrSheet = [[None for _ in range(self.numCols)] for _ in range(self.numRows)]
+        
+        for cell in lCells:
+            self.arrSheet[cell.row][cell.col] = cell.val
+
 
 
     def appendRow(self)->bool:
-        """
-        Appends an empty row to the spreadsheet.
 
-        @return True if operation was successful, or False if not.
-        """
-
-        # TO BE IMPLEMENTED
-        pass
-
-        # REPLACE WITH APPROPRIATE RETURN VALUE
+        # appends new empty row and increases total number of rows
+        self.arrSheet.append([None] * self.numCols)
+        self.numRows += 1
         return True
 
 
     def appendCol(self)->bool:
-        """
-        Appends an empty column to the spreadsheet.
 
-        @return True if operation was successful, or False if not.
-        """
-
-        # TO BE IMPLEMENTED
-        pass
-
-        # REPLACE WITH APPROPRIATE RETURN VALUE
+        # append None to the end of each row to create a new column and update number of cols
+        for row in self.arrSheet:
+            row.append(None)
+        self.numCols += 1
         return True
 
 
     def insertRow(self, rowIndex: int)->bool:
-        """
-        Inserts an empty row into the spreadsheet.
 
-        @param rowIndex Index of the existing row that will be after the newly inserted row.  If inserting as first row, specify rowIndex to be 0.  If inserting a row after the last one, specify rowIndex to be rowNum()-1.
+        # bool variable to keep track of whether row was inserted
+        inserted = False
 
-        @return True if operation was successful, or False if not, e.g., rowIndex is invalid.
-        """
-
-        # TO BE IMPLEMENTED
-        pass
-
-        # REPLACE WITH APPROPRIATE RETURN VALUE
-        return True
+        # check if index is valid
+        if (rowIndex >= 0 and rowIndex < self.numRows):
+            self.arrSheet.insert(rowIndex, [None] * self.numCols)
+            inserted = True
+        return inserted
 
 
     def insertCol(self, colIndex: int)->bool:
-        """
-        Inserts an empty column into the spreadsheet.
 
-        @param colIndex Index of the existing column that will be after the newly inserted row.  If inserting as first column, specify colIndex to be 0.  If inserting a column after the last one, specify colIndex to be colNum()-1.
+        # bool variable to keep track of whether row was inserted
+        inserted = False
 
-        return True if operation was successful, or False if not, e.g., colIndex is invalid.
-        """
-
-        # TO BE IMPLEMENTED
-        pass
-
-        # REPLACE WITH APPROPRIATE RETURN VALUE
-        return True
+        # check if index is valid
+        if (colIndex >= 0 and colIndex < self.numCols):
+            for row in self.arrSheet:
+                row.insert(colIndex, None)
+            inserted = True
+        return inserted
 
 
     def update(self, rowIndex: int, colIndex: int, value: float) -> bool:
-        """
-        Update the cell with the input/argument value.
 
-        @param rowIndex Index of row to update.
-        @param colIndex Index of column to update.
-        @param value Value to update.  Can assume they are floats.
+        # bool value to check if update has occured
+        updated = False
 
-        @return True if cell can be updated.  False if cannot, e.g., row or column indices do not exist.
-        """
+        # check if indexes are valid 
+        if (rowIndex >= 0 and rowIndex < self.numRows):
+            if (colIndex >= 0 and colIndex < self.numCols):
+                self.arrSheet[rowIndex][colIndex] = value
+                updated = True
 
-        # TO BE IMPLEMENTED
-        pass
-
-        # REPLACE WITH APPROPRIATE RETURN VALUE
-        return True
+        return updated
 
 
     def rowNum(self)->int:
-        """
-        @return Number of rows the spreadsheet has.
-        """
-
-        # TO BE IMPLEMENTED
-        pass
-
-        # REPLACE WITH APPROPRIATE RETURN VALUE
-        return 0
+        return self.numRows
 
 
     def colNum(self)->int:
-        """
-        @return Number of column the spreadsheet has.
-        """
-
-        # TO BE IMPLEMENTED
-        pass
-
-        # REPLACE WITH APPROPRIATE RETURN VALUE
-        return 0
+        return self.numCols
 
 
 
@@ -139,21 +113,28 @@ class ArraySpreadsheet(BaseSpreadsheet):
         @return List of cells (row, col) that contains the input value.
 	    """
 
-        # TO BE IMPLEMENTED
-        pass
+        # list that holds locations of cells containing desired value
+        valList = []
 
-        # REPLACE WITH APPROPRIATE RETURN VALUE
-        return []
+        # itterate through all rows and columns to check whether or not value is equal to current value
+        for i, row in enumerate(self.arrSheet):
+            for j, currentVal in enumerate(row):
+                if currentVal == value:
+                    valList.append((i,j))
+        
+        return valList
 
 
 
     def entries(self) -> [Cell]:
-        """
-        @return A list of cells that have values (i.e., all non None cells).
-        """
 
-        # TO BE IMPLEMENTED
-        pass
-
-        # TO BE IMPLEMENTED
-        return []
+        # list to hold cells that are not empty
+        nonEmptys = []
+        
+        # itterate through all rows and columns to check if cell is empty or not
+        for i, row in enumerate(self.arrSheet):
+            for j, currentVal in enumerate(row):
+                if currentVal is not None:
+                    nonEmptys.append(Cell(i,j,currentVal))
+        
+        return nonEmptys
